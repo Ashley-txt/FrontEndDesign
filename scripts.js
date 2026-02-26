a68090017cac2ae9457a90591147418af4e1711b
Ashley-txt <ashleyp1317@gmail.com>
2026-02-26 06:45:23 -0500

    done

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
-var addButton, readingButton, finishedButton, allButton, tbrButton;
-var ul;
+const addButton = document.getElementById("add");
+const enterButton = document.getElementById("enter");
+const popup = document.getElementById("popup-container");
+
+var readingButton, finishedButton, allButton, tbrButton;
+
+addButton.addEventListener("click", () => {
+    popup.classList.add("show");
+});
+
+enterButton.addEventListener("click", ()=>{
+    document.body.style.backgroundColor = "#9EA9B1";
+});
