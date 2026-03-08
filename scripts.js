ad94159446e98322efaef6a32169ccc874515880
Ashley-txt <ashleyp1317@gmail.com>
2026-03-08 17:24:12 -0400

    updated

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let title;
 let stat;
+let author;
+let genre;
 
 const books = [];
 
@@ -7,29 +9,87 @@ function popupFn(){
     document.getElementById("overlay").style.display = "block";
     document.getElementById("popupBox").style.display = "block";
 }
-//user input to insert book title and reading status -> push to array
+//submit button
 document.getElementById("submit").onclick = function(event){
     event.preventDefault();
+    //read values
     title = document.getElementById("title").value;
     stat = document.getElementById("reading-stat").value;
+    author = document.getElementById("author").value;
+    genre = document.getElementById("genre").value;
     
-    console.log(title);
-    console.log(stat);
-
-    let object = {titleofbooks: title, reading: stat};
+    //store values as obj -> add to arr
+    let object = {"titleofbooks": title, "author": author, "reading": stat, "genre": genre};
     books.push(object);
+
+    //localStorage.setItem("books", JSON.stringify(books));
+
+    //default adding all books to list
+     let list = document.getElementById('list');
+     let li = document.createElement('li');
+     li.innerText = object.titleofbooks + "\n" + object.author + "\n" + object.genre +"\n" +object.reading;
+     list.append(li);
 
     document.getElementById("overlay").style.display = "none";
     document.getElementById("popupBox").style.display = "block";    
 }
-//prints all books
-document.getElementById("all").onclick = function(){
-    let main = document.getElementById("main");
-    books.forEach(books =>{
-        let item = document.createElement('P');
-        let itemtext = document.createTextNode(`${books.titleofbooks}: ${books.reading}`);
-        item.appendChild(itemtext);
-        main.appendChild(item);
-    });
 
+//cancel button
+document.getElementById("cancel").onclick = function(){
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popupBox").style.display = "block"; 
+}
+
+//prints all books
+document.getElementById("all").onclick = function(event){
+    event.preventDefault();
+    let list = document.getElementById('list'); 
+    list.innerHTML = "";
+
+  for(let i = 0; i <= books.length; i++){
+    let li = document.createElement("li");
+    li.innerHTML = reader[i].titleofbooks + "<br>" + reader[i].author;
+    list.appendChild(li);
+  }
+}
+//filter by reading
+document.getElementById("Reading-btn").onclick = function(event){
+    event.preventDefault();
+    let list = document.getElementById('list'); 
+    list.innerHTML = "";
+    let reader = books.filter((b) => b.reading == "Reading");
+
+  for(let i = 0; i <= reader.length; i++){
+    let li = document.createElement("li");
+    li.innerHTML = reader[i].titleofbooks + "<br>" + reader[i].author;
+    list.appendChild(li);
+  }
+}
+
+document.getElementById("Planning-btn").onclick = function(event){
+    event.preventDefault();
+    let list = document.getElementById('list'); 
+    list.innerHTML = "";
+
+    let reader = books.filter((b) => b.reading == "Planning");
+
+  for(let i = 0; i <= reader.length; i++){
+    let li = document.createElement("li");
+    li.innerHTML = reader[i].titleofbooks + "<br>" + reader[i].author;
+    list.appendChild(li);
+  }
+}
+
+document.getElementById("Finished-btn").onclick = function(event){
+    event.preventDefault();
+    let list = document.getElementById('list')
+    list.innerHTML = "";
+
+    let reader = books.filter((b) => b.reading == "Finished");
+
+  for(let i = 0; i <= reader.length; i++){
+    let li = document.createElement("li");
+    li.innerHTML = reader[i].titleofbooks + "<br>" + reader[i].author;
+    list.appendChild(li);
+  }
 }
